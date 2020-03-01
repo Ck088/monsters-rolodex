c200571aa899b6d3f1dc548f9a574fe01bf2af02
CK <chetanprajapati682@gmail.com>
2020-03-01 22:28:08 +0530

    Updates

---
+++ precache-manifest.41110988cf9cac22bfaecb53869ba29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91ba438b162c60af5ab9fffe9b88db8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f4e8c7c52404f9e8b1",
+    "url": "/static/css/main.2713b8f9.chunk.css"
+  },
+  {
+    "revision": "b59f44af4d4a1bf76f80",
+    "url": "/static/js/2.215fb3a6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.215fb3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f4e8c7c52404f9e8b1",
+    "url": "/static/js/main.009a3737.chunk.js"
+  },
+  {
+    "revision": "8571fa60de2fb9bd97bc",
+    "url": "/static/js/runtime-main.6ddd9f4f.js"
+  }
+]);
--- src/components/search-box/Search-box.component.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Search-box.styles.css'
-
-export const SearchBox = ({placeholder, handleChange}) => (
-    <input type="search" className="search" placeholder={placeholder} onChange={handleChange} />
-);
